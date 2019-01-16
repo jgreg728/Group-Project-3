d1528b3818f5a4e922775c856f213cc444b4091c
Carl Singleton <Kiyocarl@gmail.com>
2019-01-15 20:29:38 -0500

    saving gitignore

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter as Router, Route} from "react-router-dom";
+import NavTabs from "./components/NavTabs";
+// import Home from "./components/pages/Home";
+// import About from "./components/pages/About";
+// import Blog from "./components/pages/Blog";
+// import Contact from "./components/pages/Contact";
+
+
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <NavTabs />
+        {/* <Route exact path="/" component={Home}/> */}
+        {/* <Route exact path="/about" component={About}/>
+        <Route exact path="/blog" component={Blog}/>
+        <Route exact path="/contact" component={Contact}/>  */}
+      </div>
+    </Router>
+  )
+}
+
+export default App;
