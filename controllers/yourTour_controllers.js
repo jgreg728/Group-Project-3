70c39efe6f81de7470380b292bfe04785152498c
jgreg728 <jjg070890@aol.com>
2019-01-17 18:58:34 -0500

    save before sharon pull request

---
+++ controllers/yourTour_controllers.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const router = express.Router();
+
+const db = require("../models");
+
+// METHODS TO EDIT DB
+module.exports = {
+  // find recent search items
+  findAll: function(req, res) {
+    
+  }
+}
--- project-3-app/controllers/yourTour_controllers.js
+++
@@ -1,5 +0,0 @@
-const express = require("express");
-
-const router = express.Router();
-
-const yourTour = require("../models/yourTour.js");
