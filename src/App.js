18426d0fbe2862da85d5796cfb8b4d8b211033f4
Carl Singleton <Kiyocarl@gmail.com>
2019-01-19 11:07:13 -0500

    update2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React from "react";
-import { BrowserRouter as Router, Route} from "react-router-dom";
-import NavTabs from "./components/NavTabs";
-// import Home from "./components/pages/Home";
-// import About from "./components/pages/About";
-// import Blog from "./components/pages/Blog";
-// import Contact from "./components/pages/Contact";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Search from "./components/pages/Search";
+import Contact from "./components/pages/Contact";
 
 
+//const App = () =>{}
 
-function App() {
+const App = () => {
   return (
     <Router>
       <div>
-        <NavTabs />
-        {/* <Route exact path="/" component={Home}/> */}
-        {/* <Route exact path="/about" component={About}/>
-        <Route exact path="/blog" component={Blog}/>
-        <Route exact path="/contact" component={Contact}/>  */}
+        <Navbar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/search" component={Search} />
+        <Route exact path="/contact" component={Contact} />
+       
+   
+       
       </div>
     </Router>
-  )
+  );
 }
 
 export default App;
